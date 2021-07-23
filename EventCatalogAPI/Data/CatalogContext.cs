6b96be688f65052f0de477c1e800796bc01dd44b
Mark Grybos <markgrybos@gmail.com>
2021-07-23 01:11:59 -0700

    Initializing project

---
+++ EventCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<EventOrganizer> EventOrganizers { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventType>(e =>
+            {
+                e.Property(t => t.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+            })
+        }
+    }
+}
---
+++ EventCatalogAPI/Domain/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Location { get; set; }
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string PictureUrl { get; set; }
+        public string Tags { get; set; }
+
+        public int EventTypeId { get; set; }
+        public int EventOrganizerId { get; set; }
+        public virtual EventType EventType { get; set; }
+        public virtual EventOrganizer EventOrganizer { get; set; }
+    }
+}
