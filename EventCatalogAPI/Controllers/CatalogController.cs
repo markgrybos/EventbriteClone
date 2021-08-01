f5510f559a1fc65082b24bc5c71c440a051912cb
Mark Grybos <markgrybos@gmail.com>
2021-08-01 12:41:11 -0700

    Tweaks

--- EventCatalogAPI/Controllers/CatalogController.cs
+++ EventCatalogAPI/Controllers/CatalogController.cs
@@ -16,52 +16,49 @@ namespace EventCatalogAPI.Controllers
     [ApiController]
     public class CatalogController : ControllerBase
     {
-        [Route("api/[controller]")]
-        [ApiController]
-        public class CatalogController : ControllerBase
+        private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
-            private readonly CatalogContext _context;
-            private readonly IConfiguration _config;
-            public CatalogController(CatalogContext context, IConfiguration config)
-            {
-                _context = context;
-                _config = config;
-            }
-            [HttpGet("[action]/{pageIndex}/{pageSize}")]
-            public async Task<IActionResult> Events(
-                [FromQuery] int pageIndex = 0,
-                [FromQuery] int pageSize = 6)
-            {
-                var eventsCount = _context.Events.LongCountAsync();
-                var events = await _context.Events
-                      .OrderBy(e => e.Name)
-                      .Skip(pageIndex * pageSize)
-                      .Take(pageSize)
-                      .ToListAsync();
-
-                events = ChangePictureUrl(events);
-                var model = new PaginatedEventsViewModels
-                {
-                    PageIndex = pageIndex,
-                    PageSize = events.Count,
-                    Count = eventsCount.Result,
-                    Data = events
-
-
-                };
-                return Ok(events);
-
-            }
-
-            private List<Event> ChangePictureUrl(List<Event> events)
-            {
-                events.ForEach(events =>
-                events.PictureUrl.Replace("http://externalcatalogtobereplaced", _config["ExternalCatalogUrl"]));
-
-                return events;
-
-            }
+            _context = context;
+            _config = config;
         }
+        [HttpGet("[action]/{pageIndex}/{pageSize}")]
+        public async Task<IActionResult> Events(
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+            var eventsCount = _context.Events.LongCountAsync();
+            var events = await _context.Events
+                  .OrderBy(e => e.Name)
+                  .Skip(pageIndex * pageSize)
+                  .Take(pageSize)
+                  .ToListAsync();
+
+            events = ChangePictureUrl(events);
+            var model = new PaginatedEventsViewModels
+            {
+                PageIndex = pageIndex,
+                PageSize = events.Count,
+                Count = eventsCount.Result,
+                Data = events
+
+
+            };
+            return Ok(events);
+
+        }
+
+        private List<Event> ChangePictureUrl(List<Event> events)
+        {
+            events.ForEach(events =>
+            events.PictureUrl.Replace("http://externalcatalogtobereplaced", _config["ExternalCatalogUrl"]));
+
+            return events;
+
+        }
+
     }
+
 }
 
